df6ea53fc181d8901fab4df343b559dd4693bd7c
Sarah Park <sypark23@pupils.nlcsjeju.kr>
2017-07-12 15:02:04 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -8,3 +8,27 @@ Asks the user for a string and prints out whether or not the string is a palindr
 
 """
 
+x= raw_input("Enter a random word")
+while len(x) >1:
+    if x[0]!=x[-1]:
+        variable=False
+        break
+    if x[0]==x[-1]:
+        x=x[1:-1]
+        x[0] == x[-1]
+        variable=True
+
+if variable == True:
+    print"the word is a palindrome"
+if variable == False:
+    print"the word is not a palindrome"
+
+
+
+
+
+
+
+
+
+
